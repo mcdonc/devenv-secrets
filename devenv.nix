8b4be30e730878e837883362d9b95126ffd489dd
Chris McDonough <chrism@plope.com>
2025-06-24 05:02:27 -0400

    cant do this will nullkeyring

--- devenv.nix
+++ devenv.nix
@@ -7,7 +7,6 @@
   secrets.profile = "testenv";
 
   enterTest = ''
-    [ "$MYSECRET" == "secret" ] || exit 1
     secretspyexe -m coverage run "$DEVENV_ROOT/test.py"
     secretspyexe -m coverage report -m --include="test.py,secrets.py"
   '';
