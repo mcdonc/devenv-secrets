7e503226a215d8518743fd5a86a373320dfbb9b3
Chris McDonough <chrism@plope.com>
2025-06-24 12:45:38 -0400

    try another test in enterShell

--- devenv.nix
+++ devenv.nix
@@ -6,6 +6,10 @@
   secrets.enable = true;
   secrets.profile = "testenv";
 
+  enterShell = ''
+    [ "$(secrets)" == "testenv" ] || exit 2
+  '';
+
   enterTest = ''
     secretspyexe -m coverage run "$DEVENV_ROOT/test.py"
     secretspyexe -m coverage report -m --include="test.py,secrets.py"
